e4cbafa37d85b38ac66f6b4f5be1c66ce02afab3
anasofiags1804 <anasofiags792@gmail.com>
2026-09-15 15:32:56 -0500

    docs: agregar comentarios explicando la validacion de voto

--- sistema.py
+++ sistema.py
@@ -2,12 +2,12 @@
 votos = {}
 
 def registrar_voto(persona, candidato):
-    
+    # Se revisa si la persona ya voto antes usando el diccionario
     if persona in votos:
         print(persona, "ya voto. No puede votar de nuevo.")
         return
 
-
+    # Si no ha votado, se guarda el nuevo voto en el diccionario
     votos[persona] = candidato
     print("Voto registrado:", persona, "voto por", candidato)
 
